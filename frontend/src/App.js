5dc7164b4f619f9338f6883597be317fb2c1858f
KoshalParwan <koshal.parwan@fasst.io>
2024-01-21 21:41:01 +0400

    feature(screen): Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className='py-3'>
+      <main className='py-3 mb-5'>
         <Container>
-          <h1>Proshop</h1>
+          {/* <h1>Proshop</h1> */}
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
