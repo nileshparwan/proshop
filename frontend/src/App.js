4b05ccf2052f7d4c235c3f55c938ad12cef304a8
KoshalParwan <koshal.parwan@fasst.io>
2024-01-21 20:40:26 +0400

    feature(ui): Header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App
