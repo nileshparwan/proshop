32a894bea01f107007dcceb606c36f307c8fc0e4
KoshalParwan <koshal.parwan@fasst.io>
2024-01-21 23:42:58 +0400

    feat(ui): React-router-bootstrap
    Change <a> to <Link>
    Amend Nav.Link to <LinkContainer>

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
-      </a>
+      <Link to={`/product/${product._id}`}>
+        <div style={{ maxHeight: '200px', width: '100%' }}>
+          <Card.Img src={product.image} variant='top' loading='lazy' />
+        </div>
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
-}
+};
 
-export default Product
+export default Product;
