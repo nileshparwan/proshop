9ee7a66195a366a760429d352992b791328dde01
KoshalParwan <koshal.parwan@fasst.io>
2024-01-22 21:59:20 +0400

    feat: backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import products from './data/products.js';
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send('listening....');
+});
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(p => p._id === req.params.id);
+    if(!req.params.id){
+        throw new Error('Product Id is missing')
+    }
+    if(!product){
+        throw new Error(`Couldn't find product for the given id ${req.params.id}`)
+    }
+    return res.json(product);
+})
+
+
+app.listen(PORT, () => console.log('Server running on port: ', PORT));
