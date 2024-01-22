c4a13752cddcdd5e25506ebcd22cea9d9a89bd13
KoshalParwan <koshal.parwan@fasst.io>
2024-01-22 10:41:22 +0400

    perf: add vercel page speed insight package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Container } from 'react-bootstrap';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -12,6 +13,7 @@ const App = () => {
         <Container>
           {/* <h1>Proshop</h1> */}
           <Outlet />
+          <SpeedInsights />
         </Container>
       </main>
       <Footer />
