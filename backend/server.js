06969a2fb150d77325887e4f544ce64241e2815a
KoshalParwan <koshal.parwan@fasst.io>
2024-01-25 14:57:00 +0400

    feat: restructure application file

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,14 @@
 import express from 'express';
 import dotEnv from 'dotenv';
-import products from './data/products.js';
-const app = express();
+import connectDB from './config/db.js';
 
-dotEnv.config();
-const PORT = process.env.PORT || 5000;
+export default () => {
+    const app = express();
+    // environment variable
+    dotEnv.config();
 
-app.get('/', (req, res) => {
-    res.send('listening....');
-});
+    // mongo db connection
+    connectDB();
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id);
-    if(!req.params.id){
-        throw new Error('Product Id is missing')
-    }
-    if(!product){
-        throw new Error(`Couldn't find product for the given id ${req.params.id}`)
-    }
-    return res.json(product);
-})
-
-
-app.listen(PORT, () => console.log('Server running on port:', PORT));
+    return app;
+};
