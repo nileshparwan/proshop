f164ea9b5200f580e6a669d06a2cb66cab02d7a6
KoshalParwan <koshal.parwan@fasst.io>
2024-02-05 22:44:42 +0400

    feat(ui): spinner css

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -3,7 +3,16 @@ import { Spinner as Spin } from 'react-bootstrap';
 
 const Spinner = () => {
   return (
-    <Spin animation='border' role='status'>
+    <Spin
+      animation='border'
+      role='status'
+      style={{
+        width: '100px',
+        height: '100px',
+        margin: 'auto',
+        display: 'block'
+      }}
+    >
       <span className='visually-hidden'>Loading...</span>
     </Spin>
   );
