8171986667f1b1c4e497e0e3745016e2ee24e812
KoshalParwan <koshal.parwan@fasst.io>
2024-02-15 22:02:35 +0400

    feat: protected and admin route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,14 +10,25 @@ import {
     getUserById,
     updateUser
 } from '../controllers/userController.js';
+import { admin, protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').get(getUsers);
-router.route('/login').post(authUser);
+router.route('/')
+    .get(protect, admin, getUsers)
+    .post(registerUser);
+
+router.route('/auth').post(authUser);
+
 router.route('/logout').post(logoutUser);
-router.route('/register').post(registerUser)
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser);
+
+router.route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
+
+router.route('/:id')
+    .delete(protect, admin, deleteUser)
+    .get(protect, admin, getUserById)
+    .put(protect, admin, updateUser);
 
 export default router;
