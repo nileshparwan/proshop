adec791782ef624735d3498e9179c97b84e7eea6
KoshalParwan <koshal.parwan@fasst.io>
2024-02-15 22:48:12 +0400

    feat: user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import generateToken from '../utils/generateToken.js';
 
 /**
  * @desc auth user
@@ -11,17 +12,11 @@ const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     const pwdIsValid = await user.matchPassword(password);
+    
     if (user && pwdIsValid) {
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' })
-        
-        // set JWT as HTTP-Only cookie
-        res.cookie('jwt', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 1000
-        })
-        
+        // set JWT as HTTP-Only cookie and token
+        generateToken(res, user._id);
+         
         res.json({
             _id: user._id,
             name: user.name,
@@ -40,7 +35,34 @@ const authUser = asyncHandler(async (req, res) => {
  * @access  Public
  */
 const registerUser = asyncHandler(async (req, res) => {
-    res.send('auth user');
+    const { name, email, password } = req.body;
+    const userExists = User.findOne({email});
+    
+    if(!userExists){
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        // set JWT as HTTP-Only cookie and token
+        generateToken(res, user._id);
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data')
+    }
 });
 
 /**
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -26,7 +26,18 @@ const userSchema = new mongoose.Schema({
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
    return await bcrypt.compare(enteredPassword, this.password)
-}
+};
+
+userSchema.pre('save', async function (next) {
+   if (!this.isModified('password')) {
+      next();
+   }
+
+   const salt = await bcrypt.genSalt(10);
+   this.password = bcrypt.hash(this.password, salt);
+});
+
+// can also do post   
 
 const User = mongoose.model('User', userSchema);
 
