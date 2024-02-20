b6ed982e57ec8dff34ad63b2bd416315d17ee45c
KoshalParwan <koshal.parwan@fasst.io>
2024-02-20 20:04:26 +0400

    fix: remove admin from getOrderById

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -16,7 +16,7 @@ router.route('/')
     .get(protect, admin, getOrders);
 
 router.route('/mine').get(protect, getMyOrders);
-router.route('/:id').get(protect, admin, getOrderById);
+router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').get(protect, updateOrderToPaid);
 router.route('/:id/deliver').get(protect, updateOrderToDelivered);
 
