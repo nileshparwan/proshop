ad469d84cd35df1b95fe44fab61494356247d500
KoshalParwan <koshal.parwan@fasst.io>
2024-03-18 14:12:49 +0400

    temp4: add deployed backend api

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://proshop-02qc.onrender.com';
-export const BASE_URL = '';
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://proshop-baaack.vercel.app';
+// export const BASE_URL = '';
 export const PRODUCT_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
