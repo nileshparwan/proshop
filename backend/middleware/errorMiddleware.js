598cc0d7d5d7c06255ae9b174ed69b2ab8f467c9
KoshalParwan <koshal.parwan@fasst.io>
2024-03-18 15:19:35 +0400

    back: commenting cast error

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,11 +8,13 @@ const errorHandler = (error, req, res, next) => {
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     let message = error.message; 
 
+    console.error(error);
+
     // check for mongoose bad OnbjectId
-    if(error.name === "CastError" && error.kind === 'ObjectId'){
-        message = 'Resource not found.';
-        statusCode = 404;
-    }
+    // if(error.name === "CastError" && error.kind === 'ObjectId'){
+    //     message = 'Resource not found.';
+    //     statusCode = 404;
+    // }
 
     res.status(statusCode).json({
         message,
