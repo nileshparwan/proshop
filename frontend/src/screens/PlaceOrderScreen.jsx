05208844704fb84592c6125d38e679f5cd4545ae
KoshalParwan <koshal.parwan@fasst.io>
2024-04-27 22:56:32 +0400

    fix(front): typo error on place order screen

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -86,7 +86,7 @@ const PlaceOrderScreen = () => {
                         </Col>
 
                         <Col>
-                          <Link to={`/products/${item.product}`}>
+                          <Link to={`/product/${item.product}`}>
                             {item.name}
                           </Link>
                         </Col>
