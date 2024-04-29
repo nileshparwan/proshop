00f0e4f5a6b06d81884b0046e47da1040285f07d
KoshalParwan <koshal.parwan@fasst.io>
2024-04-29 18:15:58 +0400

    fix(back): cast error for objectId

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,14 +8,6 @@ const errorHandler = (error, req, res, next) => {
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     let message = error.message; 
 
-    console.error(error);
-
-    // check for mongoose bad OnbjectId
-    if(error.name === "CastError" && error.kind === 'ObjectId'){
-        message = 'Resource not found.';
-        statusCode = 404;
-    }
-
     res.status(statusCode).json({
         message,
         stack: process.env.NODE_ENV === 'production'
