3b35d989ce2729e4f16bf9b572bb59af113a1d5b
KoshalParwan <koshal.parwan@fasst.io>
2024-04-29 18:31:26 +0400

    fix(back): token expiration

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,8 +8,8 @@ const generateToken = (res, userId) => {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
         sameSite: process.env.NODE_ENV !== 'development' ? 'None' : 'Strict',
-        expires: new Date(Date.now() + 86400000), // Expires in 24 hours
-        maxAge: 86400000 // Max age in milliseconds (24 hours)
+        expires: 30 * 24 * 60 * 60 * 1000, // Expires in 24 hours
+        maxAge: 30 * 24 * 60 * 60 * 1000, // Max age in milliseconds (24 hours)
     });
 };
 
